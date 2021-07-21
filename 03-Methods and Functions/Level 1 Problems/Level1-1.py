0513c5d43a4fadbc36a5fc22c87e077ba718a794
Resentful <raneljohnt@gmail.com>
2021-07-21 20:31:35 +0800

    updated

---
+++ 03-Methods and Functions/Level 1 Problems/Level1-1.py
@@ -0,0 +1,13 @@
+def old_macdonalds(a):
+    con = []
+    for x in range(len(a)):
+        if x == 0:
+            con.append(a[x].upper())
+        elif x == 3:
+            con.append(a[x].upper())
+        else:
+            con.append(a[x])
+    print(''.join(con))
+    return ''.join(con)
+
+old_macdonalds('macdonalds')
